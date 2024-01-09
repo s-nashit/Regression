e4c7c00961dfdf1dd69a779d64a7680807105b8d
Nashit Humam <85383203+s-nashit@users.noreply.github.com>
2024-01-09 14:10:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,3 +33,13 @@ def plot_regression_line(x, y, b):
   # putting labels
   plt.xlabel('x')
   plt.ylabel('y')
+
+def main():
+  # observations / data
+  x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+  y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+ 
+  # estimating coefficients
+  b = estimate_coef(x, y)
+  print("Estimated coefficients:\nb_0 = {} \
+     \nb_1 = {}".format(b
