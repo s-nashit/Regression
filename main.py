905642ff0b2496875fe9fabc7987fd5ddf2afe93
Nashit Humam <85383203+s-nashit@users.noreply.github.com>
2024-01-09 14:09:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def estimate_coef(x, y):
+  # number of observations/points
+  n = np.size(x)
+ 
+  # mean of x and y vector
+  m_x = np.mean(x)
+  m_y = np.mean(y)
+ 
+  # calculating cross-deviation and deviation about x
+  SS_xy = np.sum(y*x) - n*m_y*m_x
+  SS_xx = np.sum(x*x) - n*m_x*m_x
+ 
+  # calculating regression coefficients
+  b_1 = SS_xy / SS_xx
+  b_0 = m_y - b_1*m_x
+ 
+  return (b_0, b_1)
