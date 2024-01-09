493f15cba1ac09d27aefa2099163e5e32011724b
Nashit Humam <85383203+s-nashit@users.noreply.github.com>
2024-01-09 14:09:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,18 @@ def estimate_coef(x, y):
   b_0 = m_y - b_1*m_x
  
   return (b_0, b_1)
+
+def plot_regression_line(x, y, b):
+  # plotting the actual points as scatter plot
+  plt.scatter(x, y, color = "m",
+        marker = "o", s = 30)
+ 
+  # predicted response vector
+  y_pred = b[0] + b[1]*x
+ 
+  # plotting the regression line
+  plt.plot(x, y_pred, color = "g")
+ 
+  # putting labels
+  plt.xlabel('x')
+  plt.ylabel('y')
